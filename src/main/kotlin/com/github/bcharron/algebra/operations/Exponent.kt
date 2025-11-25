dd74d10df726c3258be356a19d1792f248031428
Benjamin Charron <bcharron@pobox.com>
2025-11-24 23:00:43 -0500

    Fix exponent toString

--- src/main/kotlin/com/github/bcharron/algebra/operations/Exponent.kt
+++ src/main/kotlin/com/github/bcharron/algebra/operations/Exponent.kt
@@ -4,6 +4,6 @@ class Exponent(l: ExprNode, r: ExprNode): BinaryNode(l, r) {
     override fun toString(): String {
         val l = if (left is ValueNode) left.toString() else "($left)"
         val r = if (right is ValueNode) right.toString() else "($right)"
-        return "$left^$right"
+        return "$l^$r"
     }
 }
