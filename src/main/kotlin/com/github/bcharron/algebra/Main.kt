4dc4d738c94e5c55be20555c498fe01231d75768
Benjamin Charron <bcharron@pobox.com>
2025-11-24 21:38:34 -0500

    Cleanup

--- src/main/kotlin/com/github/bcharron/algebra/Main.kt
+++ src/main/kotlin/com/github/bcharron/algebra/Main.kt
@@ -1,16 +1,5 @@
 package com.github.bcharron.algebra
 
-import java.util.StringTokenizer
-
-enum class Token {
-    NUMBER,
-    LEFT_PAREN,
-    RIGHT_PAREN,
-    PLUS,
-    MINUS,
-    SLASH,
-}
-
 fun main(args: Array<String>) {
     val parser = Parser()
     // val tree2 = parse(args.firstOrNull() ?: "0")
